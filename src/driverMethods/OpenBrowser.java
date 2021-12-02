80276d02fc51a430aff196bbafcd75d9ca61559a
bsin12345 <selvar5109@gmail.com>
2021-12-02 17:24:22 +0530

    WebDriverMethods

--- src/driverMethods/OpenBrowser.java
+++ src/driverMethods/OpenBrowser.java
@@ -11,22 +11,22 @@ public class OpenBrowser {
 		System.setProperty("webdriver.chrome.driver", "D:\\Users\\bs_selva\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();                //maximizing window
+		driver.manage().window().maximize();                // maximizing window
 		driver.get("https://www.facebook.com/");
-		String title = driver.getTitle();                   //to get the title of the page
+		String title = driver.getTitle();                   // to get the title of the page
 		System.out.println("Fb title: "+title);
 		String currentUrl = driver.getCurrentUrl();         // to get url of the current page
 		System.out.println("URL:" +currentUrl);
-		driver.navigate().to("https://www.amazon.sg/");     //navigating to different url
+		driver.navigate().to("https://www.amazon.sg/");     // navigating to different url
 		String amztitle = driver.getTitle();
 		System.out.println("amazon title: "+amztitle);
-		driver.navigate().back();                          //to navigate back
+		driver.navigate().back();                          // to navigate back
 		String title2 = driver.getTitle();
 		System.out.println("Fb title: "+title2);
-		driver.navigate().forward();                       //to navigate forward
+		driver.navigate().forward();                       // to navigate forward
 		String title3 = driver.getTitle();
 		System.out.println("amazon title: "+title3);
-		driver.navigate().refresh();                       //to refresh the page
+		driver.navigate().refresh();                       // to refresh the page
 		String currentUrl2 = driver.getCurrentUrl();
 		System.out.println("amazon url: "+currentUrl2);
 		driver.quit();                                     // to quit the chrome

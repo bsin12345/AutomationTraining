ed5c6a51b38f5f5063850ebdb527ac21180452c3
bs_selva <bs_selva@corp.amazonworkspaces.com>
2021-12-02 16:17:12 +0530

    WebdriverMethods

---
+++ src/driverMethods/OpenBrowser.java
@@ -0,0 +1,40 @@
+package driverMethods;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OpenBrowser {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.geccko.driver", "D:\\Users\\bs_selva\\Downloads\\FirefoxDriver\\geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+		String title = driver.getTitle();
+		System.out.println("Fb title: "+title);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println("URL:" +currentUrl);
+		driver.navigate().to("https://www.amazon.sg/");
+		String amztitle = driver.getTitle();
+		System.out.println("amazon title: "+amztitle);
+		driver.navigate().back();
+		String title2 = driver.getTitle();
+		System.out.println("Fb title: "+title2);
+		driver.navigate().forward();
+		String title3 = driver.getTitle();
+		System.out.println("amazon title: "+title3);
+		driver.navigate().refresh();
+		String currentUrl2 = driver.getCurrentUrl();
+		System.out.println("amazon url: "+currentUrl2);
+		driver.quit();
+		
+		
+		
+		
+		
+	}
+
+}
